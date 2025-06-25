796f00be0f4ee5459a44fc706df3a73c29a12766
tickkie <tickkiesoloplay@gmail.com>
2025-06-25 10:23:01 +0700

    update 25/06/2025

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -47,6 +47,18 @@ class _ItemListState extends State<HomeScreen> {
       "image":
           'https://images.unsplash.com/photo-1622644874224-7bdb61351dca?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     },
+    {
+      "type": "ขาย",
+      "title": "คอนโดหรูใจกลางทองหล่อ",
+      "location": "ทองหล่อ, กรุงเทพมหานคร",
+      "bedrooms": 3,
+      "bathrooms": 2,
+      "price": 8750000,
+      "priceUnit": "บาท",
+      "isFavorite": true,
+      "image":
+          'https://images.unsplash.com/photo-1630699144886-d6d025f5d0f1?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
   ];
 
   @override
