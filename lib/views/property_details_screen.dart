f6cdb8cb033d646f7cc3ea0a60a81188eb0edfae
tickkie <tickkiesoloplay@gmail.com>
2025-06-28 23:55:48 +0700

    update-2 28/6/25

--- lib/views/property_details_screen.dart
+++ lib/views/property_details_screen.dart
@@ -13,20 +13,61 @@ class PropertyDetailsScreen extends StatelessWidget {
       'https://images.unsplash.com/photo-1628744876497-eb30460be9f6?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     ];
 
+    Map<String, dynamic> property = {
+      "propertyType": "เช่า",
+      "roomType": "คอนโด",
+      "title":
+          "คอนโดพร้อมเข้าอยู่ใจกลางเมืองงงงงงงงงงงงงงงงงงงงงงงงงงงงงงงงงงงง",
+      "location": "อโศก, กรุงเทพมหานคร",
+      "bedrooms": 2,
+      "bathrooms": 1,
+      "area": 35,
+      "floor": 7,
+      "price": 25000,
+      "priceUnit": "บาท / เดือน",
+      "isFavorite": false,
+      "image":
+          'https://images.unsplash.com/photo-1719884630688-45ca69d68870?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      "facilities": [
+        {
+          "ฟิตเนส": true,
+          "ครัว": true,
+          "ที่จอดรถ": true,
+          "wifi": true,
+          "เลี้ยงสัตว์ได้": true,
+          "สระว่ายน้ำ": true,
+          "เฟอร์นิเจอร์": true,
+          "เครื่องปรับอากาศ": true,
+          "เครื่องซักผ้า": true,
+          "ระเบียง": true,
+        },
+      ],
+      "details":
+          "ตกแต่งครบ พร้อมเฟอร์นิเจอร์และเครื่องใช้ไฟฟ้า (แอร์, ทีวี, ตู้เย็น, ไมโครเวฟ)",
+      "created_by": "lisa",
+      "created_at": "27/06/2025",
+    };
+
     return Scaffold(
       body: SafeArea(
         child: Center(
           child: Column(
             children: [
               Container(
-                width: 300,
                 height: 300,
+                color: Colors.grey.shade200,
                 child: Swiper(
-                  pagination: SwiperPagination(),
-                  control: SwiperControl(),
-                  itemCount: 3,
+                  pagination: const SwiperPagination(),
+                  control: const SwiperControl(size: 30),
+                  itemCount: images.length,
                   itemBuilder: (context, index) {
-                    return CachedNetworkImage(imageUrl: images[index]);
+                    return CachedNetworkImage(
+                      imageUrl: images[index],
+                      placeholder: (context, url) =>
+                          const Center(child: CircularProgressIndicator()),
+                      errorWidget: (context, url, error) =>
+                          const Center(child: Icon(Icons.error)),
+                    );
                   },
                 ),
               ),
