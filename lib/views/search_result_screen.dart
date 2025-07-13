be7e0aebcaf0ae87f99294c58bdbfc2730998c32
kandanai <tickkiesoloplay@gmail.com>
2025-07-13 15:01:21 +0700

    core features finished

--- lib/views/search_result_screen.dart
+++ lib/views/search_result_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_connect/http/src/utils/utils.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:livaplace_app/controllers/search_result_controller.dart';
 import 'package:livaplace_app/views/property_card.dart';
