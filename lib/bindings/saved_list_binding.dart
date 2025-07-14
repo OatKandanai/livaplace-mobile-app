4232e7493f139c81f49049291bb092b1b7cc6bd8
kandanai <tickkiesoloplay@gmail.com>
2025-07-14 17:12:06 +0700

    saved list screen bug fix

--- lib/bindings/saved_list_binding.dart
+++ lib/bindings/saved_list_binding.dart
@@ -4,6 +4,6 @@ import 'package:livaplace_app/controllers/saved_list_controller.dart';
 class SavedListBinding extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut(() => SavedListController());
+    Get.lazyPut(() => SavedListController(), fenix: true);
   }
 }
